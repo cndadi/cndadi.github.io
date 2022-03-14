82e1403fb1c2afc5bd0a68f3d4b3a77f9ba0e5de
haiyong <1836360247@qq.com>
2022-03-14 17:04:06 +0800

    Site updated: 2022-03-14 17:04:05

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "1",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "left": "10",
+    "top": "10",
+    "key": "bfdee201fde044f99d9d4c4ddef647c5"
+  }
+}
