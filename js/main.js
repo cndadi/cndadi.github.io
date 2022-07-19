7cbb491f1f5d7b704b8a1affc29038801075e925
haiyong <1836360247@qq.com>
2022-07-19 10:41:54 +0800

    Site updated: 2022-07-19 10:41:53

--- js/main.js
+++ js/main.js
@@ -777,3 +777,10 @@ document.addEventListener('DOMContentLoaded', function () {
   refreshFn()
   unRefreshFn()
 })
+
+import { quotes } from "https://haiyong.site/js/quotes.js";
+let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+console.log(
+  `%c✨ ${randomQuote}`,
+  "font-size:20px; background:#FFF; color:#581845;padding:10px; border: 3px solid #581845;border-radius:10px;"
+);
