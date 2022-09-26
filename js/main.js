3763df08e9b12b8d87d06eb28aa1df09ffccee31
haiyong <1836360247@qq.com>
2022-09-26 22:46:02 +0800

    Site updated: 2022-09-26 22:46:00

--- js/main.js
+++ js/main.js
@@ -777,3 +777,12 @@ document.addEventListener('DOMContentLoaded', function () {
   refreshFn()
   unRefreshFn()
 })
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    audio: [{
+        name: 'name',
+        artist: 'artist',
+        url: 'url.mp3',
+        cover: 'cover.jpg'
+    }]
+});
